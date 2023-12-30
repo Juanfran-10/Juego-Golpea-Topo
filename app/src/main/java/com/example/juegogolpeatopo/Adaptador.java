7766dbb1bbb230ad96d6ded682f4510693c5cd57
juanfran <juanfriiillaa10@gmail.com>
2023-12-30 15:56:20 +0100

    Third commit

--- app/src/main/java/com/example/juegogolpeatopo/Adaptador.java
+++ app/src/main/java/com/example/juegogolpeatopo/Adaptador.java
@@ -1,11 +1,14 @@
 package com.example.juegogolpeatopo;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -15,27 +18,31 @@ import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class Adaptador extends RecyclerView.Adapter<Adaptador.MyHolder>{
+public class Adaptador extends RecyclerView.Adapter<Adaptador.MyHolder> {
     private Context context;
     private List<Usuario> usuarioList;
+    private Typeface tf;
 
     //Constructor
     public Adaptador(Context context, List<Usuario> usuarioList) {
         this.context = context;
         this.usuarioList = usuarioList;
+        this.tf = Typeface.createFromAsset(context.getAssets(), "fuentes/topo.TTF");
     }
 
-    public class MyHolder extends RecyclerView.ViewHolder{
+    public class MyHolder extends RecyclerView.ViewHolder {
         CircleImageView imagenJugador;
-        TextView nombreJugador, correoJugador, puntosJugador;
+        TextView nombreJugador, correoJugador, puntosJugador, edadJugador, paisJugador;
 
-        public MyHolder(@NonNull View itemView){
+        public MyHolder(@NonNull View itemView) {
             super(itemView);
             //Inicializamos
             imagenJugador = itemView.findViewById(R.id.imagenJugador);
             nombreJugador = itemView.findViewById(R.id.nombreJugador);
             correoJugador = itemView.findViewById(R.id.correoJugador);
             puntosJugador = itemView.findViewById(R.id.puntosJugador);
+            edadJugador = itemView.findViewById(R.id.edadJugador);
+            paisJugador = itemView.findViewById(R.id.paisJugador);
         }
     }
 
@@ -50,25 +57,53 @@ public class Adaptador extends RecyclerView.Adapter<Adaptador.MyHolder>{
     @Override
     public void onBindViewHolder(@androidx.annotation.NonNull MyHolder holder, int i) {
         //Obtenemos los datos del modelo
-        String imagen = usuarioList.get(i).getImagen();
-        String nombre = usuarioList.get(i).getNombre();
-        String correo = usuarioList.get(i).getCorreo();
+        final String imagen = usuarioList.get(i).getImagen();
+        final String nombre = usuarioList.get(i).getNombre();
+        final String correo = usuarioList.get(i).getCorreo();
+        final String edad = usuarioList.get(i).getEdad();
+        final String pais = usuarioList.get(i).getPais();
         int topos = usuarioList.get(i).getTopos();
         //Convertimos a String los topos
-        String textoTopos = String.valueOf(topos);
+        final String textoTopos = String.valueOf(topos);
 
         //Datos del jugador
         holder.nombreJugador.setText(nombre);
         holder.correoJugador.setText(correo);
         holder.puntosJugador.setText(textoTopos);
+        holder.edadJugador.setText(edad);
+        holder.paisJugador.setText(pais);
+
+        //Cambiamos el tipo de letra
+        holder.nombreJugador.setTypeface(tf);
+        holder.correoJugador.setTypeface(tf);
+        holder.puntosJugador.setTypeface(tf);
+        holder.edadJugador.setTypeface(tf);
+        holder.paisJugador.setTypeface(tf);
 
         //Imagen del jugador
-        try{
+        try {
             //Si el usuario tiene foto de perfil
             Picasso.get().load(imagen).into(holder.imagenJugador);
-        }catch (Exception e){
+        } catch (Exception e) {
             //Si no tiene foto de perfil
         }
+
+        //Al hacer click en un jugador
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetalleJugador.class);
+
+                intent.putExtra("Imagen", imagen);
+                intent.putExtra("Nombre", nombre);
+                intent.putExtra("Correo", correo);
+                intent.putExtra("Puntos", textoTopos);
+                intent.putExtra("Pais", pais);
+                intent.putExtra("Edad", edad);
+
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/juegogolpeatopo/DetalleJugador.java
@@ -0,0 +1,68 @@
+package com.example.juegogolpeatopo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class DetalleJugador extends AppCompatActivity {
+
+    private ImageView imagenDetalle;
+    private TextView titulo, nombreDetalle, correoDetalle, edadDetalle, paisDetalle, puntosDetalle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_jugador);
+
+        //Inicializamos la variables
+        imagenDetalle = findViewById(R.id.imagenDetalle);
+        titulo = findViewById(R.id.titulo);
+        nombreDetalle = findViewById(R.id.nombreDetalle);
+        correoDetalle = findViewById(R.id.correoDetalle);
+        edadDetalle = findViewById(R.id.edadDetalle);
+        paisDetalle = findViewById(R.id.paisDetalle);
+        puntosDetalle = findViewById(R.id.puntosDetalles);
+
+        String imagen = getIntent().getStringExtra("Imagen");
+        String nombre = getIntent().getStringExtra("Nombre");
+        String correo = getIntent().getStringExtra("Correo");
+        String edad = getIntent().getStringExtra("Edad");
+        String pais = getIntent().getStringExtra("Pais");
+        String puntos = getIntent().getStringExtra("Puntos");
+
+        nombreDetalle.setText("Nombre -> " + nombre);
+        correoDetalle.setText("Correo -> " + correo);
+        edadDetalle.setText("Edad -> " + edad);
+        paisDetalle.setText("Pais -> " + pais);
+        puntosDetalle.setText("Puntuacion -> " + puntos);
+
+        //Gestionamos la imagen
+        try {
+            Picasso.get().load(imagen).into(imagenDetalle);
+        } catch (Exception e) {
+            Picasso.get().load(R.drawable.mario).into(imagenDetalle);
+        }
+
+        //Cambio de letra
+        cambioDeFuente();
+
+    }
+
+    private void cambioDeFuente() {
+        //Cambiamos la letra
+        String ubicacion = "fuentes/topo.TTF";
+        Typeface tf = Typeface.createFromAsset(DetalleJugador.this.getAssets(), ubicacion);
+
+        titulo.setTypeface(tf);
+        nombreDetalle.setTypeface(tf);
+        correoDetalle.setTypeface(tf);
+        edadDetalle.setTypeface(tf);
+        paisDetalle.setTypeface(tf);
+        puntosDetalle.setTypeface(tf);
+    }
+}
